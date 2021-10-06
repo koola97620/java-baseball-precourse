31af437f1a453afecbaf9515639d33f107332a29
koola97620 <koola97620@nate.com>
2021-10-06 18:48:18 +0900

    feat: 숫자 못 맞히면 다시 숫자 입력 기능 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,22 +4,24 @@ import baseball.domain.BaseballGame;
 import baseball.domain.GameNumberGenerator;
 import baseball.domain.GameNumbers;
 import baseball.domain.Results;
+import baseball.exception.BaseballGameFailureException;
 import baseball.util.NumberUtils;
 import baseball.view.InputView;
 import baseball.view.PrintView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
         InputView inputView = new InputView();
         PrintView printView = new PrintView();
         BaseballGame baseballGame = new BaseballGame(new GameNumberGenerator());
 
-        GameNumbers numbers = GameNumbers.of(NumberUtils.toList(inputView.inputNumbers()));
-        Results results = baseballGame.start(numbers);
+        while(true) {
+            GameNumbers numbers = GameNumbers.of(NumberUtils.toList(inputView.inputNumbers()));
+            Results results = baseballGame.start(numbers);
 
-        printView.print(results.createResultsMessage());
+            printView.print(results.createResultsMessage());
 
+        }
 
     }
 }
