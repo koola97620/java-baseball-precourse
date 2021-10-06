8569062112fa478ee9d58604ab256a4d64ae7332
koola97620 <koola97620@nate.com>
2021-10-06 22:05:33 +0900

    test: 테스트 케이스 추가

--- src/test/java/baseball/domain/GameNumbersTest.java
+++ src/test/java/baseball/domain/GameNumbersTest.java
@@ -2,8 +2,8 @@ package baseball.domain;
 
 import baseball.exception.BaseballGameFailureException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,7 +13,7 @@ class GameNumbersTest {
 
     @DisplayName("숫자가 3개가 아니면 예외 발생")
     @ParameterizedTest
-    @ValueSource(strings = {"1","12","1234","12345"})
+    @ValueSource(strings = {"1", "12", "1234", "12345"})
     void length(String numberString) {
         assertThatThrownBy(() -> GameNumbers.of(numberString))
                 .isInstanceOf(BaseballGameFailureException.class)
@@ -22,7 +22,7 @@ class GameNumbersTest {
 
     @DisplayName("서로 다른 숫자가 아니면 예외 발생")
     @ParameterizedTest
-    @ValueSource(strings = {"222","333","122","331"})
+    @ValueSource(strings = {"222", "333", "122", "331"})
     void duplicate(String numberString) {
         assertThatThrownBy(() -> GameNumbers.of(numberString))
                 .isInstanceOf(BaseballGameFailureException.class)
@@ -30,25 +30,27 @@ class GameNumbersTest {
     }
 
     @DisplayName("gameNumbers 에 포함된 숫자 존재 여부 확인")
-    @Test
-    void containNumber() {
-        Number number = Number.of(5,0);
-        GameNumbers gameNumbers = GameNumbers.of("458");
+    @ParameterizedTest
+    @CsvSource(value = {"5,0,458,true", "8,1,812,true", "3,0,412,false", "5,0,132,false", "9,2,912,true"})
+    void containNumber(int number, int position, String numbers, boolean expected) {
+        Number inputNumber = Number.of(number, position);
+        GameNumbers gameNumbers = GameNumbers.of(numbers);
 
-        boolean result = gameNumbers.isContainNumber(number);
+        boolean result = gameNumbers.isContainNumber(inputNumber);
 
-        assertThat(result).isTrue();
+        assertThat(result).isEqualTo(expected);
     }
 
-    @DisplayName("gameNumbers 에 포함되고 위치가 동일한 숫자 존재 여부 확인")
-    @Test
-    void containNumberAndSamePosition() {
-        Number number = Number.of(5,0);
-        GameNumbers gameNumbers = GameNumbers.of("568");
+    @DisplayName("gameNumbers 에 포함되고 숫자값과 위치가 동일한 숫자 존재 여부 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"5,1,458,true", "8,0,812,true", "3,0,412,false", "3,1,132,true", "2,2,912,true"})
+    void containNumberAndSamePosition(int number, int position, String numbers, boolean expected) {
+        Number inputNumber = Number.of(number, position);
+        GameNumbers gameNumbers = GameNumbers.of(numbers);
 
-        boolean result = gameNumbers.isSamePosition(number);
+        boolean result = gameNumbers.isSamePosition(inputNumber);
 
-        assertThat(result).isTrue();
+        assertThat(result).isEqualTo(expected);
     }
 
 }
--- src/test/java/baseball/domain/ResultTest.java
+++ src/test/java/baseball/domain/ResultTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -7,8 +8,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResultTest {
 
+    @DisplayName("두 boolean 값에 따라 다른 Result 생성")
     @ParameterizedTest
-    @CsvSource(value = {"true,true,STRIKE","true,false,BALL","false,false,NOTHING"})
+    @CsvSource(value = {"true,true,STRIKE", "true,false,BALL", "false,false,NOTHING"})
     void create(boolean isContainNumber, boolean isSamePosition, String expected) {
         Result result = Result.of(isContainNumber, isSamePosition);
         assertThat(result).isEqualTo(Result.valueOf(expected));
